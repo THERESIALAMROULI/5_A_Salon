4c95770e28ba61b2997a152235a8c15863ffe1ed
Asima Hasyanta Sinaga <220712160@students.uajy.ac.id>
2024-09-11 09:44:59 +0700

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Asima Hasyanta Sinaga - 220712160'),
+          child: Text('Asima - 220712160'),
         ),
       ),
     );
