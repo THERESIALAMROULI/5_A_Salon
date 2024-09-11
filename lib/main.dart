0829ad3732bdece15ebf53357305df2270ac04a9
Eliandani <220711965@students.uajy.ac.id>
2024-09-11 09:40:51 +0700

    eldan change 3

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Eliandani A-220711965'),
+          child: Text('Eliandani-220711965'),
         ),
       ),
     );
