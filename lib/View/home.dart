f1b411e24afbf6530296b941772c0fdba8bd5bbb
maripang98 <220711969@students.uajy.ac.id>
2024-11-06 09:28:57 +0700

    done

---
+++ lib/View/home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:tubesfix/View/view_list.dart';
+import 'package:tubesfix/View/profile.dart';
+import 'package:tubesfix/View/home_view.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    homeView(),
+    ListNamaView(),
+    ProfileView(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+      backgroundColor: Colors.black, // <-- This works for fixed
+      selectedItemColor: Colors.white,
+      unselectedItemColor: const Color.fromARGB(255, 228, 218, 128),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home,),label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.list,),label: 'List'),
+          BottomNavigationBarItem(icon: Icon(Icons.person,),label: 'Profile'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+    );
+  }
+}
